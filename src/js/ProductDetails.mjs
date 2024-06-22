4a98c91e683e9f67b678c923363b9df36b5859db
JosueSinaca <sinaca56@gmail.com>
2024-06-21 22:35:42 -0600

    TA-Dinamic product

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,41 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+  
+  export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        await this.renderProductDetails();
+        // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+        // once we have the product details we can render out the HTML
+        // once the HTML is rendered we can add a listener to Add to Cart button
+        // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+        document.getElementById("addToCart")
+          .addEventListener("click", this.addToCart.bind(this));
+    }
+
+    addToCart() {
+        let cartList = getLocalStorage("so-cart");
+        if (!cartList) cartList = [];
+        cartList.push(this.product);
+        setLocalStorage("so-cart", cartList);
+    }
+
+    renderProductDetails () {
+        var product = document.getElementById("product").content;
+        var copyHTML = document.importNode(product, true);
+        copyHTML.querySelector(".product-brand").textContent = this.product.Brand.Name;
+        copyHTML.getElementById("NameWithoutBrand").textContent = this.product.NameWithoutBrand;
+        copyHTML.getElementById("image").src = this.product.Image;
+        copyHTML.getElementById("image").alt = this.product.NameWithoutBrand;
+        copyHTML.querySelector(".product-card__price").textContent = this.product.ListPrice;
+        copyHTML.querySelector(".product__color").textContent = this.product.Colors.ColorName;
+        copyHTML.querySelector(".product__description").innerHTML = this.product.DescriptionHtmlSimple;
+        copyHTML.getElementById("addToCart").dataset.id = this.productId;
+
+        document.getElementById("tent").appendChild(copyHTML);
+    }
+  }
--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,37 @@
-import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { setLocalStorage, getParams } from "./utils.mjs";
 
+const productId = getParams("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+
+// import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
+// import ProductData from "./ProductData.mjs";
+
+// const dataSource = new ProductData("tents");
+
+// const productId = getParams("product");
+
+// console.log(dataSource.findProductById(productId));
+
+
+// function addProductToCart(product) {
+//   var cartList = getLocalStorage("so-cart");
+//   () => {if (!cartList) cartList = []} 
+//   cartList.push(product)
+//   setLocalStorage("so-cart", cartList);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
